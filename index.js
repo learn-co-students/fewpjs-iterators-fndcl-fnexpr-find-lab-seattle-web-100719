0dd06cb92defe6a72709d7fae3d9e0a5f9a05545
alisabaj <alisabaj@gmail.com>
2019-11-26 16:49:56 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) {
+  let response = array.find(function(s) {
+    return s.result === "W"
+  })
+  if (response) {
+    return response["year"]
+  } else {
+    return undefined
+  }
+}
